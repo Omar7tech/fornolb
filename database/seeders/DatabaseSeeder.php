bbc9ccf69c06404d7ec47a86c3f36afad0dbce5c
Omar Abi Farraj <omar.7tech@gmail.com>
2026-07-13 08:18:56 +0300

    make user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-/* 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+
+
+        User::updateOrCreate([
+            'email' => 'admin@fornolb.com',
+        ], [
+            'name' => 'Admin',
+            'email' => 'admin@fornolb.com',
+            'password' => bcrypt('password'),
         ]);
- */
+
         $this->call([
             CategorySeeder::class,
             ProductSeeder::class,
