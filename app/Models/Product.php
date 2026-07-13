0d8d566a96072ec1f7f75e6b4ec29eddb80f4626
Omar Abi Farraj <omar.7tech@gmail.com>
2026-07-13 07:26:45 +0300

    make relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,21 @@
 
 namespace App\Models;
 
+use Database\Factories\ProductFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    /** @use HasFactory<ProductFactory> */
     use HasFactory;
+
+    /**
+     * @return BelongsTo<Category, $this>
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
