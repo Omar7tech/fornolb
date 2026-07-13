3d0fc88f5cdbcb58897ac87551ccf16f34de7f68
Omar Abi Farraj <omar.7tech@gmail.com>
2026-07-13 07:22:47 +0300

    make migration edits for category and product

--- database/migrations/2026_07_13_040943_create_products_table.php
+++ database/migrations/2026_07_13_040943_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,17 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('description')->nullable();
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('is_new')->default(false);
+            $table->decimal('price', 8, 2)->default(0);
+            $table->decimal('discount_price', 8, 2)->nullable();
+            $table->unsignedSmallInteger('preparation_time')->nullable();
+            $table->integer('sort')->default(0)->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->foreignIdFor(Category::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
